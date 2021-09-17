a62e3fdd89115db7ca2f98bc41fb2f64c92b7347
normidar <36730656+normidar@users.noreply.github.com>
2021-09-17 19:27:22 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter, Body
 
-from app.models.module import ApiModule, TableModule
+from app.core.module_class import TableModule, ApiModule
 
 
 class Sample(ApiModule, TableModule):
     def _register_api_bp(self, bp: APIRouter):
         @bp.get('/description')
         def description():
-            return 'this is a sample module description,' \
+            return 'this is a sample module_manager description,' \
                    ' this sample will tell you how to' \
-                   ' create a module for Figbox'
+                   ' create a module_manager for Figbox'
 
         @bp.post('/show_body')
         def show_body(body: str = Body(..., embed=True)):
